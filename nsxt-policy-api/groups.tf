053b88536f7183b9990b8ff9336ceb4115bd1944
Weien Chen <cenbiqing@gmail.com>
2023-03-03 10:50:57 +0000

    add groups for TAS gorouter, tcp_router, diego_brain

---
+++ nsxt-policy-api/groups.tf
@@ -0,0 +1,29 @@
+resource "nsxt_policy_group" "tas_gorouter" {
+  display_name = "tas-gorouter"
+  description  = "TAS gorouter VMs"
+
+  tag {
+    tag = "created_by"
+    scope = "terraform"
+  }
+}
+
+resource "nsxt_policy_group" "tas_tcp_router" {
+  display_name = "tas-tcp_router"
+  description  = "TAS TCP router VMs"
+
+  tag {
+    tag = "created_by"
+    scope = "terraform"
+  }
+}
+
+resource "nsxt_policy_group" "tas_diego_brain" {
+  display_name = "tas-diego-brain"
+  description  = "TAS diego brain VMs"
+
+  tag {
+    tag = "created_by"
+    scope = "terraform"
+  }
+}
