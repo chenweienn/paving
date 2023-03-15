ab1cc3f063ceefe95af3e013349d01f636cd13a4
Weien Chen <cenbiqing@gmail.com>
2023-03-15 06:13:01 +0000

    Let BOSH to manage the member of groups

--- nsxt-policy-api/groups.tf
+++ nsxt-policy-api/groups.tf
@@ -2,6 +2,10 @@ resource "nsxt_policy_group" "tas_gorouter" {
   display_name = "tas-gorouter"
   description  = "TAS gorouter VMs"
 
+  lifecycle {
+    ignore_changes = [criteria]
+  }
+
   tag {
     tag = "created_by"
     scope = "terraform"
@@ -12,6 +16,10 @@ resource "nsxt_policy_group" "tas_tcp_router" {
   display_name = "tas-tcp-router"
   description  = "TAS TCP router VMs"
 
+  lifecycle {
+    ignore_changes = [criteria]
+  }
+
   tag {
     tag = "created_by"
     scope = "terraform"
@@ -21,6 +29,10 @@ resource "nsxt_policy_group" "tas_tcp_router" {
 resource "nsxt_policy_group" "tas_diego_brain" {
   display_name = "tas-diego-brain"
   description  = "TAS diego brain VMs"
+
+  lifecycle {
+    ignore_changes = [criteria]
+  }
 
   tag {
     tag = "created_by"
