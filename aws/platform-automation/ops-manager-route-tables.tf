3721a386f2c460c55eca4b8320a8742dd607cd3a
WeienChen <cenbiqing@gmail.com>
2023-12-29 12:03:40 +0800

    rename route tables

--- aws/platform-automation/ops-manager-route-tables.tf
+++ aws/platform-automation/ops-manager-route-tables.tf
@@ -1,38 +1,42 @@
-resource "aws_route_table" "internet_egress_via_nat" {
+## management subnets route tables
+resource "aws_route_table" "mgmt-subnet-route-tables" {
   count  = length(var.availability_zones)
   vpc_id = aws_vpc.vpc.id
-  tags = { Name = "${var.environment_name}-rt-${count.index}" }
+  tags = { Name = "${var.environment_name}-mgmt-subnet-rt-${count.index}" }
 }
 
+# allow internet egress traffic via NAT gateway
 resource "aws_route" "nat-gateway-route" {
   count = length(var.availability_zones)
 
-  route_table_id         = element(aws_route_table.internet_egress_via_nat[*].id, count.index)
+  route_table_id         = element(aws_route_table.mgmt-subnet-route-tables[*].id, count.index)
   # nat_gateway_id         = element(aws_nat_gateway.nat[*].id, count.index)
   nat_gateway_id         = aws_nat_gateway.nat[count.index].id
   destination_cidr_block = "0.0.0.0/0"
 }
 
-# allow internet egress traffic via NAT gateway
 resource "aws_route_table_association" "route-management-subnet" {
   count          = length(var.availability_zones)
   subnet_id      = element(aws_subnet.management-subnet[*].id, count.index)
-  route_table_id = element(aws_route_table.internet_egress_via_nat[*].id, count.index)
+  route_table_id = element(aws_route_table.mgmt-subnet-route-tables[*].id, count.index)
 }
 
 
-resource "aws_route_table" "public-route-table" {
+## infra subnets route table
+resource "aws_route_table" "infra-subnet-route-table" {
   vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
+  tags = { Name = "${var.environment_name}-infra-subnet-rt" }
 }
 
 # allow internet traffic via Internet gateway
-resource "aws_route_table_association" "route-public-subnet" {
+resource "aws_route" "internet-gateway-route" {
+  route_table_id         = aws_route_table.infra-subnet-route-table.id
+  gateway_id             = aws_internet_gateway.gw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "route-infra-subnet" {
   count          = length(var.availability_zones)
   subnet_id      = element(aws_subnet.infra-subnet[*].id, count.index)
-  route_table_id = aws_route_table.public-route-table.id
+  route_table_id = aws_route_table.infra-subnet-route-table.id
 }
