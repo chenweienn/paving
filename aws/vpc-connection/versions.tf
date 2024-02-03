c72f3c338c64ac0feaabbd0d96272297277ed36a
WeienChen <cenbiqing@gmail.com>
2024-02-03 16:23:45 +0800

    remove unused providers

--- aws/vpc-connection/versions.tf
+++ aws/vpc-connection/versions.tf
@@ -4,12 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.28.0"
     }
-    random = {
-      source = "hashicorp/random"
-    }
-    tls = {
-      source = "hashicorp/tls"
-    }
   }
   required_version = "~> 1.0"
 }
